9260976cd8267b66a45b54c508f1dfaaa96d2d0e
durnf <dreadlordfluffybunny@gmail.com>
2022-07-27 14:31:53 -0230

    Blank project of FXGL

---
+++ src/main/java/com/hangman/game/HangmanApp.java
@@ -0,0 +1,16 @@
+package com.hangman.game;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class HangmanApp extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
